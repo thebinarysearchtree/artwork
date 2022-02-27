8337107357a263f48a4e7ec9a456324c7ddf7914
Andrew Jones <thebinarysearchtree@gmail.com>
2022-02-27 20:51:45 +1000

    fix createElement

--- src/proxies/html.js
+++ src/proxies/html.js
@@ -4,14 +4,12 @@ const createElement = (tag, properties) => {
   const element = document.createElement(tag);
   if (typeof properties === 'string') {
     element.innerText = properties;
+    return element;
   }
   if (!properties) {
     return element;
   }
-  for (const [key, value] of Object.entries(properties)) {
-    element[key] = value;
-  }
-  return element;
+  return Object.assign(element, properties);
 }
 
 const handler = {
